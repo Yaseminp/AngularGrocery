e3444ca19ccef7ae04689032792a7665de266ee8
Yaseminp <53197333+Yaseminp@users.noreply.github.com>
2019-08-28 22:33:44 -0700

    initial code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { GroceryComponent } from './grocery/grocery.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    GroceryComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    
+  
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [GroceryComponent]
 })
 export class AppModule { }
---
+++ src/app/grocery/grocery.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grocery',
+  templateUrl: './grocery.component.html',
+  styleUrls: ['./grocery.component.css']
+})
+export class GroceryComponent {
+  task: string;
+  tasks = [];
+
+  onClick() {
+    this.tasks.push({ name: this.task });
+    this.task = '';
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
